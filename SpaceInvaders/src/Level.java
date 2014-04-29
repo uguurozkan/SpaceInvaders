df4d47b1243e22367ad5c3f8334e07bdabdc5278
Ugur Ozkan <ugur.ozkan@ozu.edu.tr>
2014-04-29 10:48:13 +0300

    Level and Map Classes added.

---
+++ SpaceInvaders/src/Level.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+
+public class Level {
+	private Map map;
+	private ArrayList<Enemy> enemies;
+
+	public Level(Map map, ArrayList<Enemy> enemies) {
+		this.map = map;
+		this.enemies = enemies;
+	}
+
+	public Map getMap() {
+		return map;
+	}
+
+	public ArrayList<Enemy> getEnemies() {
+		return enemies;
+	}
+
+	public String toString() {
+		return enemies.size()+"";
+	}
+
+}
---
+++ SpaceInvaders/src/Map.java
@@ -0,0 +1,11 @@
+
+public class Map {
+	private char[][] charMap;
+	public Map(char[][] mapRepresentation) {
+		this.charMap = mapRepresentation;
+	}
+
+	public char[][] getCharMap() {
+		return charMap;
+	}
+}
