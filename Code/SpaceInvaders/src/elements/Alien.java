71281e8cb5ebb76589a9999a8734e6a156ea4e67
Cholpon Abdyzhaparova <cholpon.abdyzhaparova@ozu.edu.tr>
2014-05-13 01:53:33 +0300

    movement of aliens is moved into alien class

--- Code/SpaceInvaders/src/elements/Alien.java
+++ Code/SpaceInvaders/src/elements/Alien.java
@@ -2,12 +2,15 @@ package elements;
 
 import java.awt.Color;
 
+
+
 public class Alien extends Entity {
-	
+	int dir;
 
 	public Alien(Game game, int row, int column) {
 		super(game, row, column);
 		this.color=Color.BLUE;
+		this.dir=0;
 	}
 
 	@Override
@@ -17,14 +20,37 @@ public class Alien extends Entity {
 
 	@Override
 	public void goRight() {
-		// TODO Auto-generated method stub
-		
-	}
+			this.setLocation(this.getLocation().getX()+1*Entity.BASE_SIZE, this.getLocation().getY());
+		}
 
 	@Override
 	public void goLeft() {
-		// TODO Auto-generated method stub
+		this.setLocation(this.getLocation().getX()-1*Entity.BASE_SIZE, this.getLocation().getY());
 		
 	}
+	private void goDown() {
+		this.setLocation(this.getLocation().getX(), this.getLocation().getY()+1*Entity.BASE_SIZE);
+		
+	}
+	public void move(){
+		if(dir==0){
+			goRight();
+			dir=1;
+		}
+		else if(dir==1){
+			goDown();
+			dir=2;
+		}
+		else if(dir==2){
+			goLeft();
+			dir=3;
+		}
+		else if(dir==3){
+			goDown();
+			dir=0;
+		}
+	}
+
+	
 
 }
--- Code/SpaceInvaders/src/elements/Level.java
+++ Code/SpaceInvaders/src/elements/Level.java
@@ -43,21 +43,8 @@ public class Level {
 		// TODO Auto-generated method stub
 		//for all aliens move horizont then down then horizontback then down
 		if(counter>=MAX_TIME){
-			if(dir==0){
-				move(1,0);
-				dir=1;
-			}
-			else if(dir==1){
-				move(0,1);
-				dir=2;
-			}
-			else if(dir==2){
-				move(-1,0);
-				dir=3;
-			}
-			else if(dir==3){
-				move(0,1);
-				dir=0;
+			for (int i = 0; i < aliens.size(); i++) {
+				aliens.get(i).move();
 			}
 			counter=0;
 		}
@@ -67,12 +54,7 @@ public class Level {
 
 	}
 
-	private void move(int x, int y) {
-		for (int i = 0; i < aliens.size(); i++) {
-			aliens.get(i).setLocation(aliens.get(i).getLocation().getX()+x*Alien.BASE_SIZE, aliens.get(i).getLocation().getY()+y*Alien.BASE_SIZE);
-		}
-
-	}
+	
 
 	public void draw(Graphics g) {
 		for (int i = 0; i < aliens.size(); i++) {
