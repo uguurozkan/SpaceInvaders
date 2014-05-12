97e7fd2e5fe8f59f683504d0da786c0eb3775ce1
Cholpon Abdyzhaparova <cholpon.abdyzhaparova@ozu.edu.tr>
2014-05-13 01:44:20 +0300

    some sysouts deleted

--- Code/SpaceInvaders/src/elements/Level.java
+++ Code/SpaceInvaders/src/elements/Level.java
@@ -8,6 +8,7 @@ public class Level {
 	private ArrayList<Alien> aliens;
 	private int dir;
 	private int counter;
+	private static final int MAX_TIME=5;
 
 	public Level(Map map, ArrayList<Alien> aliens) {
 		this.map = map;
@@ -23,13 +24,13 @@ public class Level {
 	public ArrayList<Alien> getAliens() {
 		return aliens;
 	}
-	
+
 	public void setGameForEntities(Game game) {
 		for (Alien alien : aliens) {
 			alien.setGame(game);
 		}
 	}
-	
+
 	public void removeAlien(Alien alien) {
 		aliens.remove(alien);
 	}
@@ -41,48 +42,43 @@ public class Level {
 	public void moveAliens() {
 		// TODO Auto-generated method stub
 		//for all aliens move horizont then down then horizontback then down
-		if(counter>=5){
-		if(dir==0){
-			System.out.println(dir);
-			move(1,0);
-			dir=1;
-			
-		}
-		else if(dir==1){
-			System.out.println(dir);
-			move(0,1);
-			dir=2;
-		}
-		else if(dir==2){
-			System.out.println(dir);
-			move(-1,0);
-			dir=3;
-		}
-		else if(dir==3){
-			System.out.println(dir);
-			move(0,1);
-			dir=0;
-		}
-		counter=0;
+		if(counter>=MAX_TIME){
+			if(dir==0){
+				move(1,0);
+				dir=1;
+			}
+			else if(dir==1){
+				move(0,1);
+				dir=2;
+			}
+			else if(dir==2){
+				move(-1,0);
+				dir=3;
+			}
+			else if(dir==3){
+				move(0,1);
+				dir=0;
+			}
+			counter=0;
 		}
 		else{
 			counter++;
 		}
-		
+
 	}
 
 	private void move(int x, int y) {
 		for (int i = 0; i < aliens.size(); i++) {
 			aliens.get(i).setLocation(aliens.get(i).getLocation().getX()+x*Alien.BASE_SIZE, aliens.get(i).getLocation().getY()+y*Alien.BASE_SIZE);
 		}
-		
+
 	}
 
 	public void draw(Graphics g) {
 		for (int i = 0; i < aliens.size(); i++) {
 			aliens.get(i).draw(g);
 		}
-		
+
 	}
 
 }
