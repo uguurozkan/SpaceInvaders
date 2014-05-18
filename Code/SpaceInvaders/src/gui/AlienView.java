a3c2fdd956245cca8b0b72ef4de44bda9e727911
Ugur Ozkan <ugur.ozkan@ozu.edu.tr>
2014-05-18 23:11:30 +0300

    Alien animation added.

--- Code/SpaceInvaders/src/gui/AlienView.java
+++ Code/SpaceInvaders/src/gui/AlienView.java
@@ -1,24 +1,22 @@
 package gui;
 
-import java.awt.Point;
+import javax.swing.ImageIcon;
 
 import elements.Alien;
 
 public class AlienView extends EntityView {
-	String path = "Assets/Invaders/Invader1.png";
+	//String path = "Assets/Invaders/Invader1.png";
+	ImageIcon icon = new ImageIcon("Assets/Invaders/invaderGif.gif");
 
 	public AlienView(Alien alien) {
 		super(alien);
+		setIcon(icon);
 	}
-
 
 	@Override
 	public void move() {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
-	
 
 }
