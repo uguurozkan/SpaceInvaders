cbbbe970871953376314aed5c2ce1e6f2e7d6939
Cholpon Abdyzhaparova <cholpon.abdyzhaparova@ozu.edu.tr>
2014-05-19 16:27:00 +0300

    Bounds check done for spaceship and it is no more going out of the screen

--- Code/SpaceInvaders/src/elements/KeyInputHandler.java
+++ Code/SpaceInvaders/src/elements/KeyInputHandler.java
@@ -8,11 +8,11 @@ import java.awt.event.KeyEvent;
 
 public class KeyInputHandler extends KeyAdapter {
 	PlayerView playerView;
-	
+
 	private GamePanel gamePanel;
 	private boolean rightPressed = false;
 	private boolean leftPressed = false;
-	
+
 	public KeyInputHandler(PlayerView playerView, GamePanel gamePanel) {
 		this.playerView = playerView;
 		this.gamePanel = gamePanel;
@@ -20,12 +20,14 @@ public class KeyInputHandler extends KeyAdapter {
 
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_LEFT)
-			playerView.setLocation(playerView.getX() - 10, playerView.getY());
+			if(playerView.getX()-10>0){
+				playerView.setLocation(playerView.getX() - 10, playerView.getY());}
 		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-			playerView.setLocation(playerView.getX() + 10, playerView.getY());
+			if(playerView.getX()+10+playerView.getWidth() < gamePanel.getWidth()){
+				playerView.setLocation(playerView.getX() + 10, playerView.getY());}
 		if (e.getKeyCode() == KeyEvent.VK_SPACE)
 			gamePanel.fireBullet(playerView.getX() + playerView.getWidth() / 2, playerView.getY() - 5);
-		
+
 		playerView.repaint();
 		playerView.revalidate();
 	}
